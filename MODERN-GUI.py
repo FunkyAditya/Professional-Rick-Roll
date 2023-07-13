b8c1de17236395e7e0541ccdbb9f9d3c9e4b64c9
Aditya Prakash <114856260+FunkyAditya@users.noreply.github.com>
2023-07-13 23:15:47 +0530

    Add files via upload

---
+++ MODERN-GUI.py
@@ -0,0 +1,31 @@
+import customtkinter
+import webbrowser as chrome
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
+
+root = customtkinter.CTk()
+root.geometry("720x480")
+
+def login():
+    chrome.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System",)
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
